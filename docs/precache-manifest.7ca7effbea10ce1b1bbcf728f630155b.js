5ba5b0463a02a2cd89400ba59b1888fc5540c2d0
Luis Gomez <luis.gomez@morelia.gob.mx>
2020-09-23 15:35:30 -0500

    docs agregado

---
+++ docs/precache-manifest.7ca7effbea10ce1b1bbcf728f630155b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76dd21e27a2023ee68be1e21eaf56616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7975441d8c85fc5ea09",
+    "url": "/static/css/main.44562b27.chunk.css"
+  },
+  {
+    "revision": "ef7fe5dcd6fe2f866af2",
+    "url": "/static/js/2.c555a145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c555a145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7975441d8c85fc5ea09",
+    "url": "/static/js/main.803cc7b2.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
